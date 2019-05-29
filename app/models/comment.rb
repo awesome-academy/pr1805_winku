233f053b5e3840b82e70d89a5e64fec56c550304
Duy Nguyen <nguyenngocduy.84@live.com>
2019-05-29 20:24:14 +0700

    comment_post

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  belongs_to :user
+  belongs_to :commentable, polymorphic: true
+
+  def commentable_type=(class_name)
+    super(class_name.constantize.base_class.to_s)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,24 @@ class User < ApplicationRecord
   has_one :image, as: :imageable, dependent: :destroy
   accepts_nested_attributes_for :image, reject_if: proc {|attributes|
     attributes['image_link'].blank?}
+  has_many :comments, as: :commentable, dependent: :destroy
+
+  def self.new_with_session params, session
+    super.tap do |user|
+      if data = session["devise.facebook_data"] &&
+        session["devise.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"] if user.email.blank?
+      end
+    end
+  end
+
+  def self.from_omniauth auth
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name
+    end
+  end
 
   def self.new_with_session params, session
     super.tap do |user|
