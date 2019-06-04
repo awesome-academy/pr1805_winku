e3f74115b3e30c354c252bc53a5b9772c0658eea
PhuongThao <phuongthao.hvktqs14@gmail.com>
2019-06-04 22:12:24 +0700

    done tour

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
     get "/signin", to: "sessions#new"
     get "/timeline", to: "users#index"
     resources :statuses, only: [:create, :update, :edit, :destroy]
-    resources :tours, only: [:index, :new, :create]
     resources :posts, only: [:create, :update, :edit, :destroy] do
       resources :comments, except: :index
     end
+    resources :tours
   end
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   scope "(:locale)", locale: /en|vi/ do
