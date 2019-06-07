fd5cb9c6bd39793c52e0729da061fa08fb4963d7
PhuongThao <phuongthao.hvktqs14@gmail.com>
2019-06-07 20:53:16 +0700

    login admin

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -1,12 +1,22 @@
 class Admin::AdminController < ApplicationController
   layout "admin/application"
-  before_action :set_locale
+  before_action :set_locale, :ensure_admin!
 
   def set_locale
     I18n.locale = params[:locale] || I18n.default_locale
   end
 
   private
+
+  def ensure_admin!
+    if current_user.admin?
+      flash[:notice] = t("text.success")
+      return true
+    else
+      flash[:alert] = t("text.not_admin")
+      redirect_to root_path
+    end
+  end
 
   def default_url_options
     {locale: I18n.locale}
