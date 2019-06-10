0b49c863ade99e3b93a8fb52df91e06d37dd4a76
PhuongThao <phuongthao.hvktqs14@gmail.com>
2019-06-10 16:48:56 +0700

    admin status show, delete, update

--- app/controllers/admin/statuses_controller.rb
+++ app/controllers/admin/statuses_controller.rb
@@ -1,5 +1,38 @@
 class Admin::StatusesController < Admin::AdminController
+  before_action :load_status, only: [:show, :update, :destroy]
+
   def index
     @statuses = Status.all
+  end
+
+  def show
+  end
+
+  def update
+    if @status.block?
+      @status.update status: :unblock
+    else
+      @status.update status: :block
+    end
+    respond_to do |format|
+      format.html { redirect_back(fallback_location: root_path)}
+      format.js
+    end
+  end
+
+  def destroy
+    if @status.destroy
+      flash[:notice] = t("text.success")
+      redirect_back(fallback_location: root_path)
+    else
+      flash[:notice] = t("text.fails")
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  private
+
+  def load_status
+    @status = Status.find_by id: params[:id]
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,12 @@ class Post < ApplicationRecord
   has_one :image, as: :imageable, dependent: :destroy
   accepts_nested_attributes_for :image, reject_if: proc {|attributes|
     attributes['image_link'].blank?}
+  enum status: { block: 1, unblock: 2 }
+  before_create :set_default_status, :if => :new_record?
+
+  def set_default_status
+    self.status ||= :unblock
+  end
 
   scope :newest, -> { order created_at: :desc }
   has_many :comments, as: :commentable, dependent: :destroy
